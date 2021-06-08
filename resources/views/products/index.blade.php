abc07e7425b632656c2897fac2accd643c2a6387
angeloAlberto <angeloalberto.monteleone@gmail.com>
2021-06-08 22:14:03 +0200

    max width 1000px and margin auto added to div container

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,7 +7,7 @@
   </head>
   <body>
 
-    <div class="">
+    <div>
       @if(!session()->has('cart') || session()->get('cart', collect())->isEmpty())
         <h2>The cart is empty</h2>
       @else
@@ -18,52 +18,55 @@
         <p style="color:green">{{ session()->get('message') }}</p>
       @endif
     </div>
+    
+    <div style="max-width: 1000px; margin: auto">
+      <table class="table table-dark">
 
-    <table class="table table-dark">
-
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Name</th>
-          <th scope="col">Description</th>
-          <th scope="col">Price</th>
-          <th scope="col">Availability</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        @foreach ($products as $key => $product)
+        <thead>
           <tr>
-            <th scope="row">{{ $product['uuid'] }}</th>
-            <td>{{ $product['name'] }}</td>
-            <td>{{ $product['description'] }}</td>
-            <td>{{ $product['price'] }}</td>
-            {{-- <td>
-              @if ()
-                <span style="color:green">Disponibile</span>
-              @else
-                <span style="color:red">Non Disponibile</span>
-              @endif
-            </td> --}}
-
-            <td>
-              <a class="btn btn-primary btn-sm" href={{ route('products.show', ['product' => $product['uuid']]) }}>SEE PRODUCT</a><br>
-
-              <form method="post" id="{{ $product['uuid']. '-delete' }}" action="{{ route('products.destroy', ['product' => $product['uuid']]) }}" >
-                @method('delete')
-                @csrf
-
-                <a class="btn btn-danger btn-sm" href="#" onclick="document.getElementById('{{$product['uuid'].'-delete'}}').submit()">DELETE PRODUCT</a>
-              </form>
-
-              <form method="post" id="{{ $product['uuid']. '-cart' }}" action="{{ route('cart.addToCart', ['product' => $product['uuid']]) }}" >
-                @csrf
-                <input type="hidden" name="product" value="{{ $product['uuid'] }}">
-                <a class="btn btn-primary btn-sm" href="#" onclick="document.getElementById('{{$product['uuid'].'-cart'}}').submit()">ADD PRODUCT</a>
-              </form>
-            </td>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Price</th>
+            <th scope="col">Availability</th>
           </tr>
-        @endforeach
+        </thead>
+
+        <tbody>
+          @foreach ($products as $key => $product)
+            <tr>
+              <th scope="row">{{ $product['uuid'] }}</th>
+              <td>{{ $product['name'] }}</td>
+              <td>{{ $product['description'] }}</td>
+              <td>{{ $product['price'] }}</td>
+              {{-- <td>
+                @if ()
+                  <span style="color:green">Disponibile</span>
+                @else
+                  <span style="color:red">Non Disponibile</span>
+                @endif
+              </td> --}}
+
+              <td>
+                <a class="btn btn-primary btn-sm" href={{ route('products.show', ['product' => $product['uuid']]) }}>SEE PRODUCT</a><br>
+
+                <form method="post" id="{{ $product['uuid']. '-delete' }}" action="{{ route('products.destroy', ['product' => $product['uuid']]) }}" >
+                  @method('delete')
+                  @csrf
+
+                  <a class="btn btn-danger btn-sm" href="#" onclick="document.getElementById('{{$product['uuid'].'-delete'}}').submit()">DELETE PRODUCT</a>
+                </form>
+
+                <form method="post" id="{{ $product['uuid']. '-cart' }}" action="{{ route('cart.addToCart', ['product' => $product['uuid']]) }}" >
+                  @csrf
+                  <input type="hidden" name="product" value="{{ $product['uuid'] }}">
+                  <a class="btn btn-primary btn-sm" href="#" onclick="document.getElementById('{{$product['uuid'].'-cart'}}').submit()">ADD PRODUCT</a>
+                </form>
+              </td>
+            </tr>
+          @endforeach
+        </div>
+
       </tbody>
 
     </table>
